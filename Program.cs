5f740ca383e1ecf6a920ef960f4c641585f9579e
nelsongama <nelsongama@yahoo.com>
2021-08-31 09:04:51 -0300

    Incluindo pasta primeiros passo no c#

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeirosPassos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            for( int i=10; i<=15; i++)
+            Console.WriteLine("For OK " +i);
+            }
+        
+            int contador = 1;
+            while (contador <= 5)
+                {Console.WriteLine("While OK "+ contador);
+                 contador++;              
+                }
+
+            double j = 3;
+            do
+                {
+                    Console.WriteLine("Do OK " +j);
+                    j = j*3;
+                } while (j<=90);
+            Console.WriteLine("Do OK " +j);
+        }
+    }
+}
